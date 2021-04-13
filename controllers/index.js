574935e5b2fd3fc69a1c5b6c65c8de61f2b6f2b9
C1ndyy <cindx.xu@gmail.com>
2021-04-12 21:15:07 -0600

    linked to google API

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,34 @@ module.exports = {
   index,
 };
 
-function index(req, res) {
-  res.render("index");
+//Google AutoML
+const { PredictionServiceClient } = require('@google-cloud/automl').v1;
+const client = new PredictionServiceClient();
+const projectId = '819100513794';
+const location = 'us-central1';
+const modelId = 'TCN5642511154816221184';
+const content = 'take dog for walk'
+
+async function index(req, res) {
+  // let category = await predictCategory()
+  // console.log(category)
+  res.render("newReminder");
 }
+
+
+async function predictCategory() {
+  const request = {
+      name: client.modelPath(projectId, location, modelId),
+      payload: {
+          textSnippet: {
+              content: content,
+              mimeType: 'text/plain', // Types: 'test/plain', 'text/html'
+          },
+      },
+  };
+  const [response] = await client.predict(request);
+  let category = response.payload[0].displayName
+  return category
+}
+
+
