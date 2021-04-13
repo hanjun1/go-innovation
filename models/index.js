844c99e17d5f087213b659e60b9bee636853625a
Doug Jones <th3dougler@gmail.com>
2021-04-13 11:40:19 -0400

    fix models

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.js')[env];
+
 const db = {};
 
 let sequelize;
@@ -24,7 +25,7 @@ fs
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
-
+  
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
