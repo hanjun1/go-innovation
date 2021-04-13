baa36dab4598e898ba81c4600ea6c3b6cb3fd989
Ryan Kim <hanjun1@ualberta.ca>
2021-04-12 23:46:50 -0600

    added partial views for different functionalities

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const fs = require('fs')
+require("dotenv").config();
+const fs = require("fs");
 const db = {
   name: process.env.DB_NAME,
   user: process.env.DB_USER,
@@ -8,37 +8,37 @@ const db = {
   ssl: {
     rejectUnauthorized: false,
     //server cert
-    ca: fs.readFileSync("config/pem/server-ca.pem").toString(),
+    ca: process.env.DB_SERVER_CA,
     //client key
-    key: fs.readFileSync("config/pem/client-key.pem").toString(),
+    key: process.env.DB_CLIENT_KEY,
     //client cert
-    cert: fs.readFileSync("config/pem/client-cert.pem").toString(),
+    cert: process.env.DB_CLIENT_CERT,
   },
 };
 
-module.exports={
-  "development": {
-    "username":db.user,
-    "password": db.pass,
-    "database": db.name,
-    "host": db.host,
-    "dialect": "postgres",
-    "dialectOptions": {
-        "ssl": db.ssl
-    }
+module.exports = {
+  development: {
+    username: db.user,
+    password: db.pass,
+    database: db.name,
+    host: db.host,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: db.ssl,
+    },
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
-}
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
